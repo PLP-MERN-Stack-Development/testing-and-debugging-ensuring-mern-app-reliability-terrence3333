f067a25c5d2dae577ee72b36e887693df534e22f
Terrence Ng'andu <nganduterrence0@gmail.com>
2025-11-14 00:18:13 +0200

    Create App.jsx

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Button from './components/Button';
+
+const App = () => {
+  return (
+    <div>
+      <h1>MERN Testing App</h1>
+      <Button>Click Me</Button>
+    </div>
+  );
+};
+
+export default App;
